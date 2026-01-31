e56ab72832fa96a69f22104ab5e2aafbebe4a3b8
kh4f <kh4f.dev@gmail.com>
2026-01-31 23:00:26 +0300

    refactor(rules): add barrel file for rule exports
    Introduce a barrel file in the `rules` directory to simplify and centralize rule exports.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from '@eslint/core'
 import { css } from './css-lang'
-import { indent } from './rules/indent'
+import { indent } from './rules'
 
 export default {
 	meta: { name: 'rawstyle-eslint', version: '0.0.0' },
