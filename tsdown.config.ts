66df5f8558e03392dbc8493d593c702c58a28bde
kh4f <kh4f.dev@gmail.com>
2026-02-02 22:27:18 +0300

    build(tsdown): add `rawstyle` to `noExternal` for bundling
    Include `rawstyle` in the `noExternal` array in `tsdown.config.ts` to ensure it is bundled with the project.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,5 +5,7 @@ const isProd = process.argv.includes('--prod')
 export default {
 	minify: isProd,
 	sourcemap: isProd ? false : 'inline',
+	noExternal: ['rawstyle'],
 	fixedExtension: false,
+	inlineOnly: false,
 } satisfies UserConfig
