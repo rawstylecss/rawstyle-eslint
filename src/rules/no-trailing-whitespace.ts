55827fc52843cce4958fbcc0b02dee3f4458624e
kh4f <kh4f.dev@gmail.com>
2026-02-15 18:52:02 +0300

    fix(no-trailing-whitespace): ignore trailing whitespace on last line of templates
    Skip reporting trailing whitespace when it occurs on the last line of a template literal and consists only of indentation.
    This prevents false positives for the indentation of the closing backtick in CSS-in-JS blocks.

--- src/rules/no-trailing-whitespace.ts
+++ src/rules/no-trailing-whitespace.ts
@@ -5,6 +5,9 @@ export const noTrailingWhitespace = createRule((source, context, { lineOffset, c
 
 	for (const match of source.matchAll(/^(?!\/\/).*?([ \t]+)$/gm)) {
 		const [fullLine, trailingWs] = [match[0], match[1]]
+
+		if (isTemplate && match.index + fullLine.length === source.length && !fullLine.trim().length) continue
+
 		const lineNum = source.slice(0, match.index).split('\n').length
 		const wsStartColumn = fullLine.length - trailingWs.length
 		const wsEndColumn = fullLine.length
